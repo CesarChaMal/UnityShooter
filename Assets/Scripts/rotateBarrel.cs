c38624008291dddeaff3096e0e629ee76599833e
Cesar Francisco Chavez Maldonado <cesarchamal@gmail.com>
2020-05-12 00:11:57 +0200

    Upload code for UnityShooter project.

---
+++ Assets/Scripts/rotateBarrel.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotateBarrel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Hello World");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        Debug.Log("Hello World Update");
+        float verValue = Input.GetAxis("Vertical");
+        float horValue = Input.GetAxis("Horizontal");
+        
+        Debug.Log(verValue);
+        Debug.Log(horValue);
+
+        if (verValue != 0) {
+            transform.Rotate(verValue, 0, 0);
+        }
+
+        if (horValue != 0)
+        {
+            transform.Rotate(0, horValue, 0);
+        }
+        
+    }
+}
---
+++ Assets/Scripts/shootBall.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shootBall : MonoBehaviour {
+
+    public Transform cannonBall;
+    public float speed = 10.0f;
+    public Transform initialPosition;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(cannonBall, initialPosition.position, Quaternion.identity);
+        }
+	}
+}
